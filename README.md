f5741b92ce92047dc6c1e3aed3a7686749c4b90c
Aizen <iansky3311@gmail.com>
2023-04-21 13:10:22 +0300

    done

--- README.md
+++ README.md
@@ -319,6 +319,7 @@ import howManyParks from "./parks/howManyParks";
 import MesaVerde from "./parks/MesaVerde";
 import * as RMFunctions from "./parks/RockyMountain";
 
+
 export default function ColoradoStateParks() {
   return (
     <div>
--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,29 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+//import wildlifeParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+//import * as RMFunctions from "./parks/RockyMountain"
+/*import {
+  trees as parkTrees,
+  wildlife as parkWildlife,
+} from "./parks/RockyMountain";*/
 
-function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
-}
+export default function ColoradoStateParks() {
+  //wildlifeParks(); // => "42 parks!"
+//  console.log(parkTrees);
+// > "Aspen and Pine"
+
+//parkWildlife();
+// > "Elk, Bighorn Sheep, Moose"
+
+  //RMFunctions.trees()
+  //RMFunctions.wildlife()
+
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde/>
+    </div>
+)}
+
+
